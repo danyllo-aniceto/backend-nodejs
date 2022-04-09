b9779337636812d5c9bb0d67f2303ca70f305ad4
ygor-salles <ygorsallesaniceto@gmail.com>
2022-04-09 15:06:21 -0300

    save user

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Request, Response, Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from './repository/UserRepository';
 
 const router = Router();
 
@@ -6,8 +8,21 @@ router.get('/', (req: Request, res: Response) => {
     res.status(200).json({ message: 'Hellow world' });
 });
 
-router.post('/users', (req: Request, res: Response) => {
-    res.status(200).json({ message: 'Ola mundo' });
+router.post('/users', async (req: Request, res: Response) => {
+    console.log('caiu aqui')
+    const { name, email, password, is_admin } = req.body
+    console.log(name, email, password, is_admin)
+
+    try {
+        const conectUser = getCustomRepository(UserRepository)
+        const user = conectUser.create({ name, email, password, is_admin })
+        console.log(user)
+        await conectUser.save(user)
+
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao cadastrar o usuario' })
+    }
 });
 
 export { router };
